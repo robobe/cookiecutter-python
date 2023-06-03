8ff46698666bc06dba4b559a41eca0fb90401a48
robo <robo2020@gmail.com>
2023-06-03 22:44:54 +0300

    vscode settings
    pre commit config
    create venv auto

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,7 @@
 import subprocess
 
+subprocess.call(['python', '-m', 'venv', 'venv'])
+
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
